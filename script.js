c13cda8c9ed49e6a5c480de62593e88a27dbc632
queeniewilliams <queeniepu329@gmail.com>
2021-02-08 10:59:30 -0800

    select chips

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//spinning wheel
+
 const playBtn = document.querySelector('#play')
 const spin = () => {
   const wheel = document.querySelector('.wheel')
@@ -19,3 +21,41 @@ const spin = () => {
   })
 }
 spin()
+
+//select chips
+
+let betSize = 1
+
+const bet1 = document.getElementById('bet-1')
+const bet10 = document.getElementById('bet-10')
+const bet100 = document.getElementById('bet-100')
+const bet500 = document.getElementById('bet-500')
+
+bet1.addEventListener('click', function () {
+  bet1.style.transform = 'scale(1.2)'
+  bet10.style.transform = 'scale(0.9)'
+  bet100.style.transform = 'scale(0.9)'
+  bet500.style.transform = 'scale(0.9)'
+  betSize = 1
+})
+bet10.addEventListener('click', function () {
+  bet10.style.transform = 'scale(1.2)'
+  bet1.style.transform = 'scale(0.9)'
+  bet100.style.transform = 'scale(0.9)'
+  bet500.style.transform = 'scale(0.9)'
+  betSize = 10
+})
+bet100.addEventListener('click', function () {
+  bet100.style.transform = 'scale(1.2)'
+  bet1.style.transform = 'scale(0.9)'
+  bet10.style.transform = 'scale(0.9)'
+  bet500.style.transform = 'scale(0.9)'
+  betSize = 100
+})
+bet500.addEventListener('click', function () {
+  bet500.style.transform = 'scale(1.2)'
+  bet1.style.transform = 'scale(0.9)'
+  bet10.style.transform = 'scale(0.9)'
+  bet100.style.transform = 'scale(0.9)'
+  betSize = 500
+})
